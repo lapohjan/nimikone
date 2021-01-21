63c20c6f98319412290cc0e72c629377fba0fdbf
lapohjan <59822237+lapohjan@users.noreply.github.com>
2021-01-21 15:54:43 +0200

    moved useState to beginning of function

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,39 +1,32 @@
-import React from 'react'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-import './Search.css';
+import React, { useState, useEffect } from "react";
+import "./Search.css";
 
 function Search() {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch("names.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
 
-    const getData=()=>{
-        fetch('names.json'
-        ,{
-          headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-        }
-        )
-          .then(function(response){
-            console.log(response)
-            return response.json();
-          })
-          .then(function(myJson) {
-            console.log(myJson);
-            setData(myJson)
-          });
-      }
-
-      useEffect(()=>{
-        getData()
-      },[])
-      
-      const [data,setData]=useState([]);
-
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
-      {
-       data && data.length>0 && data.map((item)=><p>{item.about}</p>)
-     }
+      {data && data.length > 0 && data.map((item) => <p>{item.about}</p>)}
     </div>
   );
 }
