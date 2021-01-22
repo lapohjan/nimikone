cd44a5fbc178c8a9e91c24939b1b7b11ec1a3dc5
lapohjan <59822237+lapohjan@users.noreply.github.com>
2021-01-22 09:13:59 +0200

    using axios to fetch data from localhost

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,50 +1,49 @@
 import React, {useState, useEffect} from "react";
-// import axios from "axios";
-
-// function Search() {
-// 	const [data, setData] = useState([])
-
-// 	console.log(data)
-
-// 	useEffect(() => {
-// 		fetchData()
-// 	}, [])
-
-// 	function fetchData() {
-// 		axios.get(./names.json)
-// 			.then(
-//         r => {
-// 				const fetchedData = r.data.acf
-// 				setData(fetchedData)
-// 			)
-//   }
-  
-//   console.log(data.names);
+import axios from "axios";
 
 function Search() {
-  const [data,setData]=useState([]);
+	const [data, setData] = useState([])
 
-  const getData=()=>{
-    fetch('names.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
-        console.log(response)
-        return response.json();
-      })
-      .then(function(nameJson) {
-        console.log(nameJson);
-        setData(nameJson)
-      });
-  }
-  useEffect(()=>{
-    getData()
-  },[])
+	console.table(data)
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	function fetchData() {
+		axios.get('http://localhost:3000/names.json')
+			.then(r => {
+				const fetchedData = r.data
+				setData(fetchedData)
+			})
+	}
+  
+  console.log(data.names);
+
+// function Search() {
+//   const [data,setData]=useState([]);
+
+//   const getData=()=>{
+//     fetch('names.json'
+//     ,{
+//       headers : { 
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//        }
+//     }
+//     )
+//       .then(function(response){
+//         console.log(response)
+//         return response.json();
+//       })
+//       .then(function(nameJson) {
+//         console.log(nameJson);
+//         setData(nameJson)
+//       });
+//   }
+//   useEffect(()=>{
+//     getData()
+//   },[])
 
   console.log(data)
   // console.log(data.names[0].name)
@@ -59,14 +58,14 @@ function Search() {
 					Everyone at Solita:
 				</p>
 				<>
-        {data.names.map((i) => <p>{i.name}</p>)}
-					{/* {Object.keys(data).map((i, index) => {
+        {/* {data.names.map((i) => <p>{i.name}</p>)} */}
+					{Object.keys(data).map((i, index) => {
 						return (
 							<div className={data} key={index}>
-								{i} : {data[i]}
+								{i} : {data[index]}
 							</div>
 						)
-					})} */}
+					})}
 				</>
 			</div>
 		</div>
