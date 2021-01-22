38d8a063867019129ed115d7f68e36a88aa98085
lapohjan <59822237+lapohjan@users.noreply.github.com>
2021-01-22 19:41:13 +0200

    data visible on screen

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,75 +1,38 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Search() {
-	const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-	console.table(data)
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-	useEffect(() => {
-		fetchData()
-	}, [])
-
-	function fetchData() {
-		axios.get('http://localhost:3000/names.json')
-			.then(r => {
-				const fetchedData = r.data
-				setData(fetchedData)
-			})
-	}
-  
-  console.log(data.names);
-
-// function Search() {
-//   const [data,setData]=useState([]);
-
-//   const getData=()=>{
-//     fetch('names.json'
-//     ,{
-//       headers : { 
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//        }
-//     }
-//     )
-//       .then(function(response){
-//         console.log(response)
-//         return response.json();
-//       })
-//       .then(function(nameJson) {
-//         console.log(nameJson);
-//         setData(nameJson)
-//       });
-//   }
-//   useEffect(()=>{
-//     getData()
-//   },[])
-
-  console.log(data)
+  function fetchData() {
+    axios.get("http://localhost:3000/names.json").then((r) => {
+      const fetchedData = r.data;
+      setData(fetchedData);
+    });
+  }
+  console.log(data);
+  console.table(data);
+  console.log(typeof data);
   // console.log(data.names[0].name)
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Nimikone</h1>
-			</header>
-			<div>
-				<p>
-					Everyone at Solita:
-				</p>
-				<>
-        {/* {data.names.map((i) => <p>{i.name}</p>)} */}
-					{Object.keys(data).map((i, index) => {
-						return (
-							<div className={data} key={index}>
-								{i} : {data[index]}
-							</div>
-						)
-					})}
-				</>
-			</div>
-		</div>
-	);
+  const names = data.map((i) => {
+    return <h1>{i.name}</h1>;
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nimikone</h1>
+      </header>
+      <div className="App-main">
+        <p>Everyone at Solita:{names}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
